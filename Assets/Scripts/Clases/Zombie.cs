2745ebf81518b0b423038ade042f44569e1c7000
Kuuragos <alejandrab.19401@gmail.com>
2019-08-08 17:28:12 -0500

    update zombie

--- Assets/Scripts/Clases/Zombie.cs
+++ Assets/Scripts/Clases/Zombie.cs
@@ -2,11 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct DatosZombie
+{
+    
+}
 public class Zombie : MonoBehaviour
 {
+    enum GustoZ
+    {
+        Cerebro,
+        Dedos,
+        Cuello,
+        Muslo,
+        Orejas,
+    }
+    public enum Comportamientos
+    {
+        Idle,
+        Moving,
+    }
     void Start()
     {
-        int color = Random.Range(1, 4);
+       int color = Random.Range(1, 4);
        switch (color)
         {
             case 1:
@@ -19,16 +36,40 @@ public class Zombie : MonoBehaviour
                 this.GetComponent<Renderer>().material.color = Color.magenta;
                 break;
         }
+        StartCoroutine(Wait(2));
     }
-}
-public struct DatosZombie
-{
-    enum GustoZ
+    IEnumerator Wait(float seconds)
     {
-        Cerebro,
-        Dedos,
-        Cuello,
-        Muslo,
-        Orejas,
+     int posicion= Random.Range(1,5);
+     float speed = 0.5f;
+     Comportamientos Comportar =(Comportamientos )Random.Range(0,2);
+     yield return new WaitForSeconds(2);
+        switch (Comportar)
+        {
+            case Comportamientos.Idle:
+                transform.position = transform.position;
+            break;
+            case Comportamientos.Moving:
+                switch (posicion)
+                {
+                    case 1:
+                        transform.position += transform.forward * speed;
+                    break;
+                    case 2:
+                        transform.position -= transform.forward * speed;
+                        break;
+                    case 3:
+                        transform.position += transform.right * speed;
+                        break;
+                    case 4:
+                        transform.position -= transform.right * speed;
+                        break;
+                }
+                
+            break;
+        }
+      StartCoroutine(Wait(2));
     }
 }
+
+
